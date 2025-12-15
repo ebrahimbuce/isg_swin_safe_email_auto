9411d3a607b649ae57f167502b50967df964a540
Ebrahim Buceta <ebrahimbuce@gmail.com>
2025-12-15 17:28:08 -0500

    optimized v0.0.9

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,22 +1,25 @@
-module.exports = {
-  apps: [{
-    name: 'swim-safe-pr',
-    script: 'dist/main.js',
-    cwd: '$APP_DIR',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '550M',  // Reiniciar si excede 500MB
-    node_args: '--max-old-space-size=350',  // Límite de heap de Node.js
-    env: {
-      NODE_ENV: 'production'
-    },
-    // Logs
-    error_file: './logs/error.log',
-    out_file: './logs/out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-    // Restart policy
-    exp_backoff_restart_delay: 100
-  }]
-};
+const { cwd } = require('node:process');
 
+module.exports = {
+  apps: [
+    {
+      name: 'swim-safe-pr',
+      script: 'dist/main.js',
+      cwd: '/home/ubuntu/isg_swin_safe_email_auto',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '550M', // Reiniciar si excede 500MB
+      node_args: '--max-old-space-size=350', // Límite de heap de Node.js
+      env: {
+        NODE_ENV: 'production',
+      },
+      // Logs
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      // Restart policy
+      exp_backoff_restart_delay: 100,
+    },
+  ],
+};
